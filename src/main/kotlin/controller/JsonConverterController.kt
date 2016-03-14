4722858453ac4705f103744d6ff79f99c700fd0e
zonehy <zonehyzou@gmail.com>
2016-03-14 22:10:54 +0800

    remove unused package

--- src/main/kotlin/controller/JsonConverterController.kt
+++ src/main/kotlin/controller/JsonConverterController.kt
@@ -5,11 +5,8 @@ import dto.User
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
-import java.util.*
 
 /**
  * Created by zonehy on 2016.3.14.
